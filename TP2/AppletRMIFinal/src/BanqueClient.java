494951224013ccd3cb74074d7c8741cb4f939ab2
JohnnyBoy <johnnyguerin@outlook.com>
2015-03-10 20:35:20 -0400

    Fini final, ajout des tests d'entrée (integer et double)

--- TP2/AppletRMIFinal/src/BanqueClient.java
+++ TP2/AppletRMIFinal/src/BanqueClient.java
@@ -12,6 +12,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JApplet;
 import javax.swing.JButton;
@@ -23,7 +24,7 @@ public class BanqueClient extends JApplet implements ActionListener {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	//Créer un compte
+	// Créer un compte
 	Button CreerCompteButton;
 
 	TextField idFieldTitle;
@@ -35,48 +36,44 @@ public class BanqueClient extends JApplet implements ActionListener {
 	TextField nomField;
 	TextField prenomField;
 	TextField soldeField;
-	//Voir les détails d'un compte
+	// Voir les détails d'un compte
 	Button detailCompteButton;
-	
+
 	TextField idDetailCompteTitle;
 	TextField idDetailCompte;
-	
-	//Ajouter une somme sur un compte
+
+	// Ajouter une somme sur un compte
 	Button ajouterSomme;
-	
+
 	TextField idAjouterSommeTitle;
 	TextField idAjouterSommeCompte;
-	
+
 	TextField montantAjoutTitle;
 	TextField montantAjout;
-	
-	//Retirer une somme d'un compte
+
+	// Retirer une somme d'un compte
 	Button retirerSomme;
-	
+
 	TextField idRetirerSommeTitle;
 	TextField idRetirerSommeCompte;
-	
+
 	TextField montantRetraitTitle;
 	TextField montantRetrait;
-	
-	
 
 	JTextArea etatApplet;
 
-	
 	public void init() {
-		
+
 		try {
 			// Création des assets de l'applet
-			setLayout(new FlowLayout(5,10,10));
-			getContentPane().setBackground( Color.orange );
-			
+			setLayout(new FlowLayout(5, 10, 10));
+			getContentPane().setBackground(Color.orange);
+
 			// Initialisation Créer compte
 			CreerCompteButton = new Button("Créer un compte");
 			CreerCompteButton.setBackground(java.awt.Color.red);
 			CreerCompteButton.addActionListener(this);
-			
-			
+
 			idFieldTitle = new TextField("ID:");
 			idFieldTitle.setBackground(Color.yellow);
 			nomFieldTitle = new TextField("Nom:");
@@ -114,72 +111,74 @@ public class BanqueClient extends JApplet implements ActionListener {
 
 			add(soldeFieldTitle);
 			add(soldeField);
-			
-			//Initialisation Voir les détails d'un compte
-			detailCompteButton = new Button("         Voir les détails d'un compte         ");
+
+			// Initialisation Voir les détails d'un compte
+			detailCompteButton = new Button(
+					"         Voir les détails d'un compte         ");
 			detailCompteButton.addActionListener(this);
 			detailCompteButton.setBackground(Color.red);
 			detailCompteButton.addActionListener(this);
-			
+
 			idDetailCompteTitle = new TextField("ID du compte à consulter:", 20);
-			idDetailCompte = new TextField("",48);
-			
-			
+			idDetailCompte = new TextField("", 48);
+
 			idDetailCompteTitle.setEnabled(false);
 			idDetailCompte.setEnabled(true);
-			
+
 			add(detailCompteButton);
 			add(idDetailCompteTitle);
 			add(idDetailCompte);
-			
-			//Initialisation Ajouter une somme sur un compte
-			ajouterSomme = new Button("      Ajouter de l'argent à un compte      ");
+
+			// Initialisation Ajouter une somme sur un compte
+			ajouterSomme = new Button(
+					"      Ajouter de l'argent à un compte      ");
 			ajouterSomme.setBackground(java.awt.Color.red);
 			ajouterSomme.addActionListener(this);
-			
-			idAjouterSommeTitle = new TextField("ID du compte à mettre à jour:",20);
-			idAjouterSommeCompte = new TextField("",5);
-			
+
+			idAjouterSommeTitle = new TextField(
+					"ID du compte à mettre à jour:", 20);
+			idAjouterSommeCompte = new TextField("", 5);
+
 			montantAjoutTitle = new TextField("Montant à ajouter:");
-			montantAjout = new TextField("",15);
-			
+			montantAjout = new TextField("", 15);
+
 			idAjouterSommeTitle.setEnabled(false);
 			idAjouterSommeCompte.setEnabled(true);
 			montantAjoutTitle.setEnabled(false);
 			montantAjout.setEnabled(true);
-					
+
 			add(ajouterSomme);
 			add(idAjouterSommeTitle);
 			add(idAjouterSommeCompte);
 			add(montantAjoutTitle);
 			add(montantAjout);
-						
-			
-			//Initialisation Retirer une somme d'un compte
 
-			retirerSomme = new Button("      Retirer de l'argent à un compte      ");
+			// Initialisation Retirer une somme d'un compte
+
+			retirerSomme = new Button(
+					"      Retirer de l'argent à un compte      ");
 			retirerSomme.setBackground(java.awt.Color.red);
 			retirerSomme.addActionListener(this);
-			
-			idRetirerSommeTitle = new TextField("ID du compte à mettre à jour:",20);
-			idRetirerSommeCompte = new TextField("",5);
+
+			idRetirerSommeTitle = new TextField(
+					"ID du compte à mettre à jour:", 20);
+			idRetirerSommeCompte = new TextField("", 5);
 			montantRetraitTitle = new TextField("Montant à retirer:");
-			montantRetrait = new TextField("",15);
-			
+			montantRetrait = new TextField("", 15);
+
 			idRetirerSommeTitle.setEnabled(false);
 			idRetirerSommeCompte.setEnabled(true);
 			montantRetraitTitle.setEnabled(false);
 			montantRetrait.setEnabled(true);
-					
+
 			add(retirerSomme);
 			add(idRetirerSommeTitle);
 			add(idRetirerSommeCompte);
 			add(montantRetraitTitle);
 			add(montantRetrait);
-			
-			
+
 			// Rétroaction sur l'état de l'applet
-			etatApplet = new JTextArea("Applet démarré!", 25,50);
+			etatApplet = new JTextArea("Applet démarré!", 25, 50);
 			etatApplet.setEditable(false);
 			etatApplet.setBackground(Color.LIGHT_GRAY);
 			add(etatApplet);
@@ -205,26 +204,41 @@ public class BanqueClient extends JApplet implements ActionListener {
 				String nouveauPrenom;
 				double nouveauSolde;
 
-				nouveauId = Integer.parseInt(idField.getText());
-				nouveauNom = nomField.getText();
-				nouveauPrenom = prenomField.getText();
-				nouveauSolde = Double.parseDouble(soldeField.getText());
+				if (isInteger(idField.getText())
+						&& isDouble(soldeField.getText())) {
+					nouveauId = Integer.parseInt(idField.getText());
+					nouveauNom = nomField.getText();
+					nouveauPrenom = prenomField.getText();
+					nouveauSolde = Double.parseDouble(soldeField.getText());
+					remoteReference.creerCompte(nouveauId, nouveauNom,
+							nouveauPrenom, nouveauSolde);
+					etatApplet
+							.setText("Compte créé avec succès avec les valeurs suivantes: "
+									+ "\n"
+									+ "ID: "
+									+ nouveauId
+									+ "\n"
+									+ "Nom: "
+									+ nouveauNom
+									+ "\n"
+									+ "Prenom: "
+									+ nouveauPrenom
+									+ "\n"
+									+ "Solde: "
+									+ nouveauSolde + "\n");
+				}
 
-				remoteReference.creerCompte(nouveauId, nouveauNom,
-						nouveauPrenom, nouveauSolde);
+				else {
+					etatApplet
+							.setText("Transaction annulé, veuillez recommencer avec des formats d'entrées valides.");
 
-				etatApplet.setText("Compte créé avec succès avec les valeurs suivantes: " + "\n"
-									+ "ID: " + nouveauId + "\n"
-									+ "Nom: " + nouveauNom + "\n"
-									+ "Prenom: " + nouveauPrenom + "\n"
-									+ "Solde: " + nouveauSolde + "\n");
-
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		else if(evt.getSource() == detailCompteButton){
+
+		else if (evt.getSource() == detailCompteButton) {
 			try {
 				// établissement de la connexion RMI
 				int port = 8989;
@@ -236,33 +250,50 @@ public class BanqueClient extends JApplet implements ActionListener {
 
 				int detailId;
 
-				detailId = Integer.parseInt(idDetailCompte.getText());
+				if (isInteger(idDetailCompte.getText())) {
+					detailId = Integer.parseInt(idDetailCompte.getText());
+					Compte detailDuCompte = new Compte();
+					detailDuCompte = remoteReference.afficherCompte(detailId);
+					etatApplet.setText("");
+					etatApplet
+							.setText("Voici les détails du compte: "
+									+ "\n"
+									+ "ID: "
+									+ detailDuCompte.getId()
+									+ "\n"
+									+ "Nom: "
+									+ detailDuCompte.getNom()
+									+ "\n"
+									+ "Prenom: "
+									+ detailDuCompte.getPrenom()
+									+ "\n"
+									+ "Solde: "
+									+ detailDuCompte.getSolde()
+									+ "\n"
+									+ "-----------------------  Transactions  ------------------------------- \n");
+					ArrayList<Transaction> afficherListe = detailDuCompte
+							.getListeTransaction();
+					for (int i = 0; i < afficherListe.size(); i++) {
+						Transaction afficherTransaction = afficherListe.get(i);
+						etatApplet.append("Montant de la transaction #"
+								+ (i + 1) + ": "
+								+ afficherTransaction.getMontant() + "$"
+								+ " solde: " + afficherTransaction.getSolde()
+								+ " effectué le: "
+								+ afficherTransaction.getDate() + "\n");
+					}
+				}
 
-				Compte detailDuCompte = new Compte();
-				detailDuCompte = remoteReference.afficherCompte(detailId);
-				etatApplet.setText("");
-				etatApplet.setText("Voici les détails du compte: " + "\n"
-					+ "ID: " + detailDuCompte.getId() + "\n"
-					+ "Nom: " + detailDuCompte.getNom() + "\n"
-					+ "Prenom: " + detailDuCompte.getPrenom() + "\n"
-					+ "Solde: " + detailDuCompte.getSolde() + "\n"
-					+ "-----------------------  Transactions  ------------------------------- \n"
-					);
-				ArrayList<Transaction> afficherListe = detailDuCompte.getListeTransaction();
-				for(int i=0; i < afficherListe.size(); i++)
-				{
-					Transaction afficherTransaction = afficherListe.get(i);
-					etatApplet.append("Montant de la transaction #" + (i+1) + ": " + afficherTransaction.getMontant() + "$"
-							+ " solde: " + afficherTransaction.getSolde()
-							+ " effectué le: " + afficherTransaction.getDate()+ "\n");		
+				else {
+					etatApplet
+							.setText("Transaction annulé, veuillez recommencer avec des formats d'entrées valides.");
 				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-		}
-		else if(evt.getSource() == ajouterSomme){
+
+		} else if (evt.getSource() == ajouterSomme) {
 			try {
 				// établissement de la connexion RMI
 				int port = 8989;
@@ -275,28 +306,38 @@ public class BanqueClient extends JApplet implements ActionListener {
 				int idAjoutSomme;
 				double ajouterNouvelleSomme;
 
-				idAjoutSomme = Integer.parseInt(idAjouterSommeCompte.getText());
-				ajouterNouvelleSomme = Double.parseDouble(montantAjout.getText());
+				if (isInteger(idAjouterSommeCompte.getText()) // Vérifie le
+																// format des
+																// données
+						&& isDouble(montantAjout.getText())) {
+					idAjoutSomme = Integer.parseInt(idAjouterSommeCompte
+							.getText());
+					ajouterNouvelleSomme = Double.parseDouble(montantAjout
+							.getText());
 
-				if(remoteReference.ajoutSomme(idAjoutSomme, ajouterNouvelleSomme))
-				{
+					if (remoteReference.ajoutSomme(idAjoutSomme,
+							ajouterNouvelleSomme)) {
 
-					etatApplet.setText("Solde ajouté avec succès: " + "\n"
-										+ "ID: " + idAjoutSomme + "\n"
-										+ "Solde ajouté: " + ajouterNouvelleSomme + "\n");					
+						etatApplet.setText("Solde ajouté avec succès: " + "\n"
+								+ "ID: " + idAjoutSomme + "\n"
+								+ "Solde ajouté: " + ajouterNouvelleSomme
+								+ "\n");
+					} else {
+						etatApplet
+								.setText("La transaction à été annulé, un problème est survenu.");
+					}
 				}
-				else
-				{
-					etatApplet.setText("La transaction à été annulé, un problème est survenu.");	
+
+				else {
+					etatApplet
+							.setText("Transaction annulé, veuillez recommencer avec des formats d'entrées valides.");
+
 				}
-
-
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-		}
-		else if(evt.getSource() == retirerSomme){
+
+		} else if (evt.getSource() == retirerSomme) {
 			try {
 				// établissement de la connexion RMI
 				int port = 8989;
@@ -309,27 +350,65 @@ public class BanqueClient extends JApplet implements ActionListener {
 				int idRetirerSomme;
 				double retirerNouvelleSomme;
 
-				idRetirerSomme = Integer.parseInt(idRetirerSommeCompte.getText());
-				retirerNouvelleSomme = Double.parseDouble(montantRetrait.getText());
+				if (isInteger(idRetirerSommeCompte.getText()) // Vérifie le
+																// format des
+																// données
+						&& isDouble(montantRetrait.getText())) {
+					idRetirerSomme = Integer.parseInt(idRetirerSommeCompte
+							.getText());
+					retirerNouvelleSomme = Double.parseDouble(montantRetrait
+							.getText());
 
-				if(remoteReference.retirerSomme(idRetirerSomme, retirerNouvelleSomme))
-				{
+					etatApplet
+							.setText("Veuillez recommencer avec des formats valides");
 
-					etatApplet.setText("Solde retiré avec succès: " + "\n"
-										+ "ID: " + idRetirerSomme + "\n"
-										+ "Solde retiré: " + retirerNouvelleSomme + "\n");
+					if (remoteReference.retirerSomme(idRetirerSomme,
+							retirerNouvelleSomme)) {
+						etatApplet.setText("Solde retiré avec succès: " + "\n"
+								+ "ID: " + idRetirerSomme + "\n"
+								+ "Solde retiré: " + retirerNouvelleSomme
+								+ "\n");
+					} else {
+						etatApplet
+								.setText("La transaction à été annulé, un problème est survenu.");
+					}
+
 				}
-				else
-				{
-					etatApplet.setText("La transaction à été annulé, un problème est survenu.");	
-				}
 
+				else {
+					etatApplet
+							.setText("Transaction annulé, veuillez recommencer avec des formats d'entrées valides.");
+				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
+	}
+
+	public static boolean isInteger(String s) { // Code basé sur ce lien
+												// http://www.tutorialspoint.com/java/util/scanner_hasnextint.htm
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(s.trim());
+		if (!sc.hasNextInt())
+			return false;
+		// we know it starts with a valid int, now make sure
+		// there's nothing left!
+		sc.nextInt();
+		return !sc.hasNext();
+	}
+
+	public static boolean isDouble(String s) { // Code basé sur ce lien
+												// http://www.tutorialspoint.com/java/util/scanner_hasnextint.htm
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(s.trim());
+		if (!sc.hasNextDouble())
+			return false;
+		// we know it starts with a valid double, now make sure
+		// there's nothing left!
+		sc.nextDouble();
+		return !sc.hasNext();
 	}
 }
